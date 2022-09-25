479e0e37b49f0c4e537522449dec16c4ba346470
Tom Griesbach <Tom.Griesbach@gmx.de>
2022-09-25 11:50:48 +0200

    Add Basic function to arithmetic class

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,3 +1,21 @@
 public class Arithmetic {
-    
+    public double addition(double x, double y) {
+        return x + y;
+    }
+
+    public double subtraction(double x, double y) {
+        return x - y;
+    }
+
+    public double multiply(double x, double y) {
+        return x * y;
+    }
+
+    public double division(double x, double y) {
+        if(y == 0.0) {
+            return 0;
+        } else {
+            return x / y;
+        }
+    }
 }
